b4eb25592fb071e094f8f6549a51e1e43ecf47bc
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-05-26 17:53:28 +0600

    error solved 404

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,30 +1,29 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = ({order}) => {
+const CheckoutForm = ({ order }) => {
   const stripe = useStripe();
   const elements = useElements();
   const [cardError, setCardError] = useState('');
   const [success, setSuccess] = useState('');
   const [clientSecret, setClientSecret] = useState('');
   const price = order?.orderValue;
-  console.log(order);
 
-  useEffect( () => {
+  useEffect(() => {
     fetch("https://pc-hub-bd.herokuapp.com/create-payment-intent", {
       method: 'POST',
       headers: {
-          'content-type': 'application/json',
-          authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        'content-type': 'application/json',
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`
       },
-      body: JSON.stringify({price})
+      body: JSON.stringify({ price })
     })
-    .then(res => res.json())
-    .then(data => {
-      if(data?.clientSecret){
-        setClientSecret(data.clientSecret)
-      }
-    })
+      .then(res => res.json())
+      .then(data => {
+        if (data?.clientSecret) {
+          setClientSecret(data.clientSecret)
+        }
+      })
   }, [price]);
 
   const handleSubmit = async (event) => {
@@ -50,7 +49,7 @@ const CheckoutForm = ({order}) => {
       setCardError(error?.message || '');
 
       // confirm card payment
-      const {paymentIntent, error: intentError} = await stripe.confirmCardPayment(
+      const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
         clientSecret,
         {
           payment_method: {
@@ -63,44 +62,45 @@ const CheckoutForm = ({order}) => {
         },
       );
 
-      if(intentError){
+      if (intentError) {
         setCardError(intentError.message)
-      }else {
-      setSuccess('[PaymentMethod]', paymentMethod);
+      } else {
+        setSuccess('[PaymentMethod]', paymentMethod);
+      }
+
+
     }
-
-
-  }
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <CardElement
-          options={{
-            style: {
-              base: {
-                fontSize: '16px',
-                color: '#424770',
-                '::placeholder': {
-                  color: '#aab7c4',
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: '16px',
+                  color: '#424770',
+                  '::placeholder': {
+                    color: '#aab7c4',
+                  },
+                },
+                invalid: {
+                  color: '#9e2146',
                 },
               },
-              invalid: {
-                color: '#9e2146',
-              },
-            },
-          }}
-        />
-        <div className="pt-8">
-          <button className='btn btn-accent text-white px-7' type="submit" disabled={!stripe || !clientSecret}>
-            Pay
-          </button>
-        </div>
-      </form>
-      {
-        cardError && <p className='text-red-700'>{cardError}</p>
-      }
-    </>
-  );
+            }}
+          />
+          <div className="pt-8">
+            <button className='btn btn-accent text-white px-7' type="submit" disabled={!stripe || !clientSecret}>
+              Pay
+            </button>
+          </div>
+        </form>
+        {
+          cardError && <p className='text-red-700'>{cardError}</p>
+        }
+      </>
+    );
+  };
 };
 
 export default CheckoutForm;
